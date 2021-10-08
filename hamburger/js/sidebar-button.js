a929a5b5189a4d722edf06b53e548d3c2da9e2f4
yoshino(ri) <44309517+y0shin0ri@users.noreply.github.com>
2021-10-09 00:30:57 +0900

    fastcommit

---
+++ hamburger/js/sidebar-button.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#drawer_toggle").click(function () {
+    $("#drawer_menu").toggleClass("open");
+    $("#overlay").toggleClass("overlay-open");
+    $("#close").toggleClass("close-isopen");
+    $("#menu").toggleClass("menu-isclose");
+    $(""), toggleClass("");
+  });
+});
