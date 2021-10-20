14ca6ede273f496c135a1bb2f271978a83e7479f
yoshino(ri) <44309517+y0shin0ri@users.noreply.github.com>
2021-10-20 22:58:31 +0900

    10-20footerOK

---
+++ hamburger/hamburger/js/sidebar-button.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#drawer_toggle").click(function () {
+    $("#drawer_menu").toggleClass("open");
+    $("#overlay").toggleClass("overlay-open");
+    $("#close").toggleClass("close-isopen");
+    $("#menu").toggleClass("menu-isclose");
+    $("#sidebar").toggleClass("sidebar-isopen");
+    $(""), toggleClass("");
+  });
+});
